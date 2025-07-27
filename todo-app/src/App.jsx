7304a78b6fba58315db17033d755429ec734e41d
emudassar <contacmudassar@gmail.com>
2025-07-27 16:35:26 +0500

    adding the checkbox for task completion

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
 
   const addTodo = () => {
-    if(input.trim() !== '') {
-      setTodos([...todos, input])
-      setInput('')
+    if (input.trim() !== "") {
+      const task = {
+        name: input,
+        isComplete: false,
+      };
+      setTodos([...todos, task]);
+      setInput("");
     }
   };
 
+  const toggleTodo = (index) => {
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, isComplete: !todo.isComplete } : todo
+      )
+    );
+  };
+
   const removeItem = (indexToRemove) => {
-    setTodos(todos.filter((_, index) => index !== indexToRemove))
-  }
+    setTodos(todos.filter((_, index) => index !== indexToRemove));
+  };
 
   return (
     <div className="body">
@@ -24,11 +37,29 @@ const App = () => {
             type="text"
             value={input}
             onChange={(e) => setInput(e.target.value)}
+            placeholder="Add a task"
           />
-          <button className="add-button" onClick={addTodo}>Add</button>
+          <button className="add-button" onClick={addTodo}>
+            Add
+          </button>
           <ul>
             {todos.map((todo, index) => (
-              <li key={index}>{todo} <button className="delete-button" onClick={() => removeItem(index)}>Delete</button></li>
+              <li key={index} className={todo.isComplete ? "completed" : ""}>
+                <div className="task-content">
+                  <input
+                    type="checkbox"
+                    checked={todo.isComplete}
+                    onChange={() => toggleTodo(index)}
+                  />
+                  <span className="task-text">{todo.name}</span>
+                </div>
+                <button
+                  className="delete-button"
+                  onClick={() => removeItem(index)}
+                >
+                  Delete
+                </button>
+              </li>
             ))}
           </ul>
         </div>
