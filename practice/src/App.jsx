023be79f5425cca8af3c4432ef2f7be389cbb261
emudassar <contacmudassar@gmail.com>
2025-07-29 13:29:46 +0500

    practice props by buildidng simple components by passing name

---
+++ practice/src/App.jsx
@@ -0,0 +1,12 @@
+
+import WelcomeMessage from './components/WelcomeMessage'
+
+const App = () => {
+  return (
+    <div>
+      <WelcomeMessage name="Sara!"/>
+    </div>
+  )
+}
+
+export default App
---
+++ practice/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+
+const WelcomeMessage = (props) => {
+  return (
+    <div>
+      <h1>Welcome back, {props.name}</h1>
+    </div>
+  )
+}
+
+export default WelcomeMessage
