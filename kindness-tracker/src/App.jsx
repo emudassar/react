950bacc859f9dcd42283c021366b9c1deb3c8976
emudassar <contacmudassar@gmail.com>
2025-07-29 12:54:22 +0500

    building kindness tracker to practice react daily

---
+++ kindness-tracker/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="main-div">
+      <div className="container">
+        <h1>Kindness Tracker</h1>
+        <div className="inner-div">
+          <input type="text" />
+          <button>Add Act</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
